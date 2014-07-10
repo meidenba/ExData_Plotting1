8acdc927d8b3e621e26418494c15db2f8fdca9a1
meidenba <meidenba@bc.edu>
2014-07-10 16:59:57 -0400

    Create Plot 3

---
+++ plot3.R
@@ -0,0 +1,28 @@
+#plot 2
+#Gather and Transform Data
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="power.zip")
+PowerConsumption = read.table(unz("power.zip","household_power_consumption.txt"),sep=";", header=TRUE, stringsAsFactors=FALSE)
+DateTimeFunction=function(x,y) {paste(x,y,sep=" ")}
+
+#Process Data
+PowerConsumption$Date_Time1=mapply(DateTimeFunction,PowerConsumption$Date,PowerConsumption$Time)
+PowerConsumption$Date_Time=strptime(PowerConsumption$Date_Time1,format="%d/%m/%Y %H:%M:%S")
+PowerConsumption$Date=strptime(PowerConsumption$Date, format="%d/%m/%Y")
+PowerConsumption$Global_active_power=as.numeric(PowerConsumption$Global_active_power)
+PowerConsumption$Global_intensity=as.numeric(PowerConsumption$Global_intensity)
+PowerConsumption$Global_reactive_power=as.numeric(PowerConsumption$Global_reactive_power)
+PowerConsumption$Sub_metering_1=as.numeric(PowerConsumption$Sub_metering_1)
+PowerConsumption$Sub_metering_2=as.numeric(PowerConsumption$Sub_metering_2)
+PowerConsumption$Sub_metering_3=as.numeric(PowerConsumption$Sub_metering_3)
+PowerConsumption$Voltage=as.numeric(PowerConsumption$Voltage)
+
+#Subset Data
+PowerConsumption=subset(PowerConsumption,PowerConsumption$Date>as.POSIXlt("2007-01-31") & PowerConsumption$Date<as.POSIXlt("2007-02-03"))
+
+#Create Plot
+png(file="plot3.png",width=480,height=480)
+plot(PowerConsumption$Date_Time,PowerConsumption$Sub_metering_1, type="l", xlab="",ylab="Energy sub metering")
+lines(PowerConsumption$Date_Time,PowerConsumption$Sub_metering_2,col="red")
+lines(PowerConsumption$Date_Time,PowerConsumption$Sub_metering_3,col="blue")
+legend("topright",lty=1,col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
